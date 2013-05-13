33c6d02c9c3895bc81cf1ef14f647761197d6e09
james2doyle <james2doyle@gmail.com>
2013-05-12 23:55:51 -0400

    added gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['gruntfile.js', 'js/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          browser: true,
+          document: true
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/**/*.js'],
+        dest: 'dist/js/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    clean: ['<%= concat.dist.dest %>'],
+    stylus: {
+      compile: {
+        options: {
+          paths: ['css'],
+          // use: [require('css/__init')],
+          import: ['../node_modules/nib/lib/nib']
+        },
+        files: {
+          'dist/css/style.css': ['css/*.styl']
+        }
+      }
+    },
+    // watch: {
+    //   files: ['<%= jshint.files %>', 'css/*.styl', 'dist/index.html'],
+    //   tasks: ['jshint','concat','uglify', 'clean', 'stylus']
+    // }
+    watch: {
+      scripts: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint', 'concat'],
+        options: {
+          nospawn: true
+        }
+      },
+      styles: {
+        files: ['css/*.styl'],
+        tasks: ['stylus'],
+        options: {
+          // nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint','concat','uglify', 'clean', 'stylus']);
+
+};
