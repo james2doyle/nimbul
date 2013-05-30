f8a0799ebd6f2dbd1cb926d74717ef2728eb73f9
james2doyle <james2doyle@gmail.com>
2013-05-30 15:20:06 -0400

    add imagemin taks and svgmin task

--- gruntfile.js
+++ gruntfile.js
@@ -2,74 +2,96 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     clean: ['<%= concat.dist.dest %>', 'dist/js/scripts.min.js'],
-    jshint: {
-      files: ['gruntfile.js', 'js/**/*.js', '!js/_modernizr.custom.min.js'],
-      options: {
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          browser: true,
-          document: true
-        }
+    imagemin: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'img',
+          src: '{,*/}*.{png,jpg,jpeg}',
+          dest: 'dist/img'
+        }]
       }
     },
-    concat: {
+    svgmin: {
       options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['js/*.js'],
-        dest: 'dist/js/scripts.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        plugins: [{
+          removeViewBox: false
+        }]
       },
       dist: {
         files: {
-          'dist/js/scripts.min.js': ['<%= concat.dist.dest %>']
+          'dist/img/*.svg': 'img/*.svg'
         }
-      }
-    },
-    stylus: {
-      compile: {
+      },
+      jshint: {
+        files: ['gruntfile.js', 'js/**/*.js', '!js/_modernizr.custom.min.js'],
         options: {
-          paths: ['css'],
-          // use: [require('css/__init')],
-          import: ['../node_modules/nib/lib/nib']
+          globals: {
+            jQuery: true,
+            console: true,
+            module: true,
+            browser: true,
+            document: true
+          }
+        }
+      },
+      concat: {
+        options: {
+          separator: ';'
         },
-        files: {
-          'dist/css/style.css': ['css/*.styl']
-        }
-      }
-    },
-    watch: {
-      scripts: {
-        files: ['<%= concat.dist.src %>'],
-        tasks: ['jshint', 'concat'],
-        options: {
-          // nospawn: true
+        dist: {
+          src: ['js/*.js'],
+          dest: 'dist/js/scripts.js'
         }
       },
-      styles: {
-        files: ['css/*.styl'],
-        tasks: ['stylus'],
+      uglify: {
         options: {
-          // nospawn: true
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        },
+        dist: {
+          files: {
+            'dist/js/scripts.min.js': ['<%= concat.dist.dest %>']
+          }
+        }
+      },
+      stylus: {
+        compile: {
+          options: {
+            paths: ['css'],
+            // use: [require('css/__init')],
+            import: ['../node_modules/nib/lib/nib']
+          },
+          files: {
+            'dist/css/style.css': ['css/*.styl']
+          }
+        }
+      },
+      watch: {
+        scripts: {
+          files: ['<%= concat.dist.src %>'],
+          tasks: ['jshint', 'concat'],
+          options: {
+            // nospawn: true
+          }
+        },
+        styles: {
+          files: ['css/*.styl'],
+          tasks: ['stylus'],
+          options: {
+            // nospawn: true
+          }
         }
       }
-    }
-  });
-// add image min
+    });
+  // add image min
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-svgmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['clean','jshint','concat','uglify','stylus']);
+  grunt.registerTask('default', ['clean','svgmin','imagemin', 'jshint', 'concat', 'uglify', 'stylus']);
 
-};
+  };
